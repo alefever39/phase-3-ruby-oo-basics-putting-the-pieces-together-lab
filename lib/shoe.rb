f3ba70230a1f6b472c71a04743deae80b43d776e
Allie LeFever <alefever39@gmail.com>
2022-06-27 14:24:07 -0600

    commit tree 61d85f0fe61ce73c0effd88b981108a8516d4437

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition, :brand
+  def initialize(brand)
+    self.send("brand=", brand)
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
